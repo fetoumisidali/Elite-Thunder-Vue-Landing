210e8de4e497183099356f2d39b4e38cb910608f
Sidali Fetoumi <fetoumisidali@gmail.com>
2025-11-09 01:32:51 +0100

    fix: adjust controller image reference for proper functionality

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -85,8 +85,8 @@ onUnmounted(() => {
         </div>
       </div>
       <div class="relative order-1 lg:order-2">
-        <div class="relative" ref="controllerContainer">
-          <img src="../../assets/controller.png"
+        <div class="relative" >
+          <img ref="controllerContainer" src="../../assets/controller.png"
             class="animate-float animate-thunderPulse z-20 relative w-full h-auto max-w-xl object-contain transform hover:scale-105 transition-transform duration-500"
             style="filter: drop-shadow(0 0 20px rgba(251, 191, 36, 0.4)) drop-shadow(0 0 40px rgba(251, 191, 36, 0.2))">
         </div>
